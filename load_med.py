404252fd2b627cbf2d670f378fe8580fe0236d97
Vadym Zhernovyi <zhernovoy.vadim@apriorit.com>
2019-03-15 11:41:30 +0200

    added data loader for med data

---
+++ load_med.py
@@ -0,0 +1,36 @@
+
+import os
+import cv2
+import numpy as np
+
+from keras import backend as K
+from keras.utils import np_utils
+
+from data import DataLoader
+
+NB_TRAIN_SAMPLES = 3000 # 3000 training samples
+NB_VALID_SAMPLES = 100 # 100 validation samples
+NUM_CLASSES = 10
+TRAIN_FOLDER = os.path.abspath('./med_dataset/train_emb')
+TEST_FOLDER = os.path.abspath('./med_dataset/test_emb')
+
+def load_med_data(img_rows, img_cols):
+    # Load our training and validation sets
+    train_loader = DataLoader(TRAIN_FOLDER)
+    test_loader = DataLoader(TEST_FOLDER)
+    (X_train, Y_train) = train_loader.load_data()
+    (X_valid, Y_valid) = test_loader.load_data()
+
+    # Resize trainging images
+    if K.image_dim_ordering() == 'th':
+        X_train = np.array([cv2.resize(img.transpose(1,2,0), (img_rows,img_cols)).transpose(2,0,1) for img in X_train[:NB_TRAIN_SAMPLES,:,:,:]])
+        X_valid = np.array([cv2.resize(img.transpose(1,2,0), (img_rows,img_cols)).transpose(2,0,1) for img in X_valid[:NB_VALID_SAMPLES,:,:,:]])
+    else:
+        X_train = np.array([cv2.resize(img, (img_rows,img_cols)) for img in X_train[:NB_TRAIN_SAMPLES,:,:,:]])
+        X_valid = np.array([cv2.resize(img, (img_rows,img_cols)) for img in X_valid[:NB_VALID_SAMPLES,:,:,:]])
+
+    # Transform targets to keras compatible format
+    Y_train = np_utils.to_categorical(Y_train[:NB_TRAIN_SAMPLES], NUM_CLASSES)
+    Y_valid = np_utils.to_categorical(Y_valid[:NB_VALID_SAMPLES], NUM_CLASSES)
+
+    return X_train, Y_train, X_valid, Y_valid
